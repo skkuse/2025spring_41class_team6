62eb216cf69d53c1efc8f9c8e7314d87f51181e4
byungKHee <byungheekang@g.skku.edu>
2025-05-29 13:09:30 +0900

    feat: api 대체 함수 임시 제작

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -15,6 +15,10 @@ const router = createBrowserRouter([
     path: "/history",
     element: <History />,
   },
+  {
+    path: "/history/:id",
+    element: <History />,
+  },
 ]);
 
 export default router;
