00855d655b44022fd527ee747584bcde0abfa141
softkau <paul3143@naver.com>
2025-06-08 08:42:41 +0900

    cleanup: API endpoint 리팩토링
    API 엔드포인트들이 리팩토링되었습니다.
    main.py에 있던 모든 엔드포인트들이
    * auth.py
    * chatrooms.py
    * movies.py
    로 이동했습니다.
    
    기존의 /api/user GET 엔드포인트는 /api/auth/user GET으로 이동하였으나,
    호환성을 위해 Redirection 처리하였습니다.

--- backend/src/api_schema.py
+++ backend/src/api_schema.py
@@ -87,3 +87,44 @@ class MovieIDRequest(BaseModel):
 class ArchiveRequest(BaseModel):
     movie_id: int
     rating: int
+
+from database.utils import MovieInfoInternal, PersonInfoInternal, CharacterInfoInternal
+def public_movie_info(internal: MovieInfoInternal) -> Movie:
+    from common.tmdb_utils import tmdb_full_image_path, ImgType
+
+    movie = internal
+
+    return Movie(
+        id=movie.id,
+        title=movie.title,
+        overview=movie.tmdb_overview                             if movie.tmdb_overview else "[TMDB 줄거리 없음]",
+        wiki_document=movie.wiki_document                        if movie.wiki_document else "[WIKIPEDIA 정보 없음]",
+        release_date=str(movie.release_date)                     if movie.release_date else "[방영일 정보 없음]",
+        poster_img_url=tmdb_full_image_path(movie.poster_img_url, ImgType.POSTER, None)  if movie.poster_img_url  else "",
+        trailer_img_url=tmdb_full_image_path(movie.trailer_img_url, ImgType.STILL, None) if movie.trailer_img_url else "",
+        bookmarked=movie.bookmarked                                                      if movie.bookmarked is not None else False,
+        rating = movie.rating,
+        ordering = 0,
+        genres = movie.genres,
+        characters = [public_character_info(chara) for chara in movie.characters],
+        directors = [public_person_info(crew) for crew in movie.directors]
+    )
+
+def public_person_info(internal: PersonInfoInternal) -> Person:
+    from common.tmdb_utils import tmdb_full_image_path, ImgType
+
+    return Person(
+        id = internal.id,
+        name = internal.name,
+        profile_image = tmdb_full_image_path(
+            internal.profile_image_path,
+            ImgType.PROFILE,
+            None) if internal.profile_image_path else ""
+    )
+
+def public_character_info(internal: CharacterInfoInternal) -> Character:
+    return Character(
+        id = internal.id,
+        name = internal.name,
+        actor = public_person_info(internal.actor) if internal.actor else None
+    )
